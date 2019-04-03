e5c6421456eeb4c22992f56404d9ca0274967909
Munene Kiruja <enenum@yahoo.com>
2019-04-03 01:29:49 -0700

    rename folders with punctiation, added more camps, added more images

--- gen_podcasts.py
+++ gen_podcasts.py
@@ -20,16 +20,18 @@ camps = [
     ('Victory in Laodicea', 'C14_'),
     ('What Is Your Life', 'C15_'),
     ('All Out', 'C16_'),
+    ('Allos - Another of the Kind', 'C17_', 'allos.jpg'),
     ('Grace n peace', 'C18_'),
     ('Life In The Church', 'C19_'),
     ('The Mega Church', 'C20_'),
-    ('The Mystries Of God', 'C21_'),
+    ('The Mysteries Of God', 'C21_'),
     ('Zogreo', 'C22_'),
     ('Agree on the Way', 'C23_'),
     ('HOW TO SURVIVE IN EPHESUS', 'C24_'),
     ('The Presence', 'C25_'),
     ('Kruptos Man', 'C26_'),
     ('Gates and Roads', 'C27_'),
+    ('Bema', 'C28_'),
     ('Victory In Pergamos', 'C29_'),
     ('Barrenness and Fruitfulness', 'C30_'),
     ('Preparation of The Gospel', 'C31_'),
@@ -37,26 +39,34 @@ camps = [
     ('OTHERS', 'C32_'),
     ('church planting', 'C34_'),
     ('Perfection', 'C35_'),
-    ('Busselization', 'C40_'),
+    ('Snake Junction', 'C36_'),
+    ('Australia 1000', 'C37_'),
+    ('Take Up Your Cross', 'C38_'),
+    ('Birthday - Kee Waa', 'C39_', 'BIRTHDAY%20(KEEWAA).jpg'),
+    ('Busselization', 'C40_', 'BUSSELISATION.jpg'),
     ('Obedience Unto Death', 'C41_'),
+    ('Become Who You Can Become', 'C42_'),
+    ('Spiritual Battles', 'C43_'),
     ('Apocalypse', 'C44_'),
     ("The Lord's Anointed", 'C45_'),
     ('Do The Work Of An Evangelist', 'C46_'),
-    ('I and My Children', 'C47_'),
+    ('I and My Children', 'C47_', 'I%20AND%20THE%20CHILDREN.jpg'),
     ('Missions', 'C48_'),
+    ('The Calling', 'C50_'),
     ('Tell Them', 'C51_'),
     ('The Sufferings of Christ', 'C52_'),
-    ('The Powers of a Cross', 'C53_'),
+    ('The Powers of a Cross', 'C53_', '1504183776_POWERS%20OF%20A%20CROSS.jpg'),
     ('Warfare Keys', 'C55_'),
     ('Finish What You Started', 'C56_'),
     ('Mighty Foundations', 'C57_'),
     ('My First Love', 'C58_'),
+    ('Coming Out of Obscurity', 'C59_'),
     ('Lay Power', 'C60_'),
-    ('The Blessings of Abraham', 'C61_'),
+    ('The Blessings of Abraham', 'C61_', 'THE%20BLESSING%20OF%20ABRAHAM.jpg'),
     ('Predestination', 'C62_'),
-    ('Why You Are Not a Missionary', 'C63_'),
-    ('Tasters or Partakers', 'C64_'),
-    ('Privilage', 'C65_'),
+    ('Why You Are Not a Missionary', 'C63_', 'WHY%20ARE%20YOU%20NOT%20A%20MISSIONARY.jpg'),
+    ('Tasters or Partakers', 'C64_', 'Tasters%20&amp;%20Partakers.jpg'),
+    ('The Privilege', 'C65_', 'The%20Priviledge.jpg'),
     ('The Bag of Seeds', 'C66_'),
     ('If You Love The Lord', 'C67_'),
     ('That my house may be filled', 'C68_'),
@@ -66,17 +76,55 @@ camps = [
     ('Lord I know you need somebody', 'C72_'),
     ('Inexorability in the Mission', 'C73_'),
     ("God's Banquet", 'C74_'),
+    ('Many Are Called', 'C75_'),
     ('The Word of my Patience', 'C76_'),
     ('Be thou faithful unto death', 'C77_'),
-    ('The Sweet influences of The Holy Spirit', 'C78_'),
-    ('Secrets of the Anointing and His Anointing', 'C79_'),
+    ('The Sweet influences of The Holy Spirit', 'C78_', 'sweet%20influences.jpg'),
+    ('Secrets of the Anointing and His Anointing', 'C79_', '1544765559_the%20secret.jpg'),
+    ('Atmosphere', 'C80_', 'atmosphere.jpg'),
+    ('One Hundred Million Souls', 'C81_', '100-Million-Souls.jpg'),
     ('The beautiful Job', 'C82_'),
     ('Principles of war', 'C83_'),
-    ("Mission America 'There must be Missions'", 'C87_'),
-    ('Mission Europe', 'C88_'),
-    ("Mission SA 'Prepare the way of the Lord'", 'C89_'),
-    ("Mission Africa 'The ministry of the sower'", 'C90_'),
-    ('Who is he that overcomes the world', 'C91_')
+    ('Fight The Good Fight', 'C84_'),
+    ('Wise As Serpents', 'C85_'),
+    ('Give Thyself Wholly', 'C86_', 'give%20thyself%20.jpg'),
+    ("Mission America 'There must be Missions'", 'C87_', 'mission%20america.jpg'),
+    ('Mission Europe', 'C88_', 'MISSION%20EUROPE.jpg'),
+    ("Mission SA 'Prepare the way of the Lord'", 'C89_', 'mission%20south%20africa.jpg'),
+    ("Mission Africa 'The ministry of the sower'", 'C90_', '1498920239_Mission-Africa-Dag-Heward-Mills.jpg'),
+    ('Who is he that overcomes the world', 'C91_'),
+    ('God Requireth That Which Is Past', 'C92_'),
+    ('How Can I Say Thanks', 'C93_'),
+    ('Fulfil Your Ministry', 'C94_'),
+    ('Obligation of Christians', 'C95_', 'Obligation%20_of_Christians.jpg'),
+    ('Attempt Great Things', 'C96_'),
+    ('Expect Great Things', 'C97_'),
+    ('Controlling The Flesh', 'C98_'),
+    ('Let My People Go', 'C99_'),
+    ('Victory In Laodecia', 'C100_', '1503520278_VICTORY%20IN%20LAODICEA.jpg'),
+    ('You Are Supernatural', 'C101_'),
+    ('Where Is The Flock That Was Given Thee?', 'C102_', 'where%20is%20the%20flock%3f.jpg'),
+    ('Why We Must Have A Mega Church', 'C103_'),
+    ('The Destiny of the Church', 'C104_'),
+    ('Army of Hard Followers', 'C105_'),
+    ('Bearing Fruit', 'C106_'),
+    ('Reasons For The Breakup', 'C107_'),
+    ('Arise And Shine', 'C108_'),
+    #('', ''), #109 seems to be an incomplete compilation
+    ('The Church Must Send Or It Will End', 'C110_'),
+    ('No City Shall Be Too Strong For You', 'C111_'),
+    ('Neutralize The Curse', 'C112_', 'neutralise.jpg'),
+    ('The Mystery of Our Marriage to God', 'C113_'),
+    #('', ''), #114 seems to be an incomplete collection
+    ('Everything By Prayer Nothing Without Prayer', 'C115_'),
+    ('Make Yourself a Saviour of Men', 'C116_'),
+    ('The Reward For Hard Work Is More Work', 'C117_', 'the%20reward%20for%20hard.jpg'),
+    ('No Weeping No Gnashing', 'C118_'),
+    ('Candle In The Dark', 'C119_'),
+    ('Twenty-Five To Fifty', 'C120_'),
+    ('The Isles Shall Wait For Me', 'C121_'),
+    ('Attempt Great Things For God', 'C122_'),
+    ('Life In The Church', 'C124_'),
 ]
 FILES_URL="http://daghewardmillsaudio.org/songs/"
 LINK_URL="https://biyn-tech-llc.github.io/campcast/"
@@ -152,7 +200,7 @@ body {
   display: flex;
   -ms-flex-wrap: wrap; /* IE10 */
   flex-wrap: wrap;
-  padding: 0 4px;
+  padding: 0 0px;
 }
 
 /* Create four equal columns that sits next to each other */
@@ -160,11 +208,11 @@ body {
   -ms-flex: 25%; /* IE10 */
   flex: 25%;
   max-width: 25%;
-  padding: 0 4px;
+  padding: 0 0px;
 }
 
 .column img {
-  margin-top: 8px;
+  margin-top: 0px;
   vertical-align: middle;
 }
 
@@ -185,6 +233,59 @@ body {
     max-width: 100%;
   }
 }
+
+figure {
+  text-align: center;
+  font-style: italic;
+  font-size: smaller;
+  text-indent: 0;
+  border: thin silver solid;
+  margin: 0em;
+  padding: 0.1em;
+  width: 100%;
+}
+img.scaled {
+  width: 100%;
+}
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 140px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  bottom: 75%;
+  left: 50%;
+  margin-left: -75px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip .tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+
 </style>
 <body>
 
@@ -193,6 +294,7 @@ body {
   <h1>The Camps below may be played with your podcast app - <i>if it accepts RSS feeds directly</i></h1>
   <p>Click on a camp to copy its RSS feed into the clipboard. Then paste the link into your podcast application.</p>
   <p>Most podcast players have this feature - including iTunes podcast, Podcast Addict, etc. <i>Google Play Music and Google Podcasts do not.</i></p>
+  <p><a href="https://medium.com/@joshmuccio/how-to-manually-add-a-rss-feed-to-your-podcast-app-on-desktop-ios-android-478d197a3770">This article has help</a> for adding RSS links to many podcast apps.</p>
 </div>
 
 <!-- Photo Grid -->
@@ -220,7 +322,10 @@ function fallbackCopyTextToClipboard(text) {
 
   document.body.removeChild(textArea);
 }
-function copyTextToClipboard(text) {
+function copyTextToClipboard(text, element_id) {
+  var tooltip = document.getElementById(element_id);
+  tooltip.innerHtml = "Copied: " + text;
+  
   if (!navigator.clipboard) {
     fallbackCopyTextToClipboard(text);
     return;
@@ -230,6 +335,11 @@ function copyTextToClipboard(text) {
   }, function(err) {
     console.error('Async: Could not copy text: ', err);
   });
+}
+
+function mouseOut(element_id) {
+  var tooltip = document.getElementById(element_id)
+  tooltip.innerHtml = "Click image to copy RSS feed to clipboard"
 }
 </script>
 
@@ -242,8 +352,9 @@ column_f = '''  </div>
 '''
 
 fig = '''
-    <figure onclick="copyTextToClipboard('___PODCASTRSS___')">
-      <img src="___IMAGE___" style="width:100%">
+    <figure class="tooltip" onclick="copyTextToClipboard('___PODCASTRSS___', '___CAMPID___')" onmouseout="mouseOut('___CAMPID___')">
+      <span class="tooltiptext" id="___CAMPID___">Click image to copy RSS feed to clipboard</span>
+      <img class=scaled src="___IMAGE___" style="width:100%">
       <figcaption>___TITLE___</figcaption>
     </figure>
 '''
@@ -258,20 +369,23 @@ for camp in camps:
     #print cmd
     ssns = subprocess.check_output(cmd.split())
     #print "sessions are: " + ssns
-    cmd = "grep -i -m 1 " + name.replace(' ', '.*') + " images.html"
     image = LINK_URL + 'DAG.jpg'
-    try:
-        image_line = subprocess.check_output(cmd.split())
-        image_file = re.search(r'(?<=href=").*jpg(?=">)', image_line).group(0)
-        image = IMAGES_URL + image_file
-        #print image
-        if requests.get(image).status_code != 200:
-            image = LINK_URL + 'DAG.jpg'
-    except subprocess.CalledProcessError, e:
-        print str(e) 
+    if len(camp) >= 3:
+        image = IMAGES_URL + camp[2]
+    else:
+        cmd = "grep -i -m 1 " + name.replace(' ', '.*') + " images.html"
+        try:
+            image_line = subprocess.check_output(cmd.split())
+            image_file = re.search(r'(?<=href=").*jpg(?=">)', image_line).group(0)
+            image = IMAGES_URL + image_file
+            #print image
+            if requests.get(image).status_code != 200:
+                image = LINK_URL + 'DAG.jpg'
+        except subprocess.CalledProcessError, e:
+            print str(e) 
 
     cmd = "sed -ne s/^.*href=\"\(" + expr + ".*jpg\).*$/\\1/p allsongs.html" 
-    folder = name.lower().replace(' ', '_')
+    folder = name.lower().replace(' ', '_').replace("'","")
     podcast = pod_header.replace('___TITLE___', name) \
                 .replace('___LINK___', LINK_URL + folder) \
                 .replace('___IMAGE___', image)
@@ -302,7 +416,8 @@ for camp in camps:
     # the podcast entry
     list_page += fig.replace('___PODCASTRSS___', LINK_URL + podfile) \
             .replace('___TITLE___', name) \
-            .replace('___IMAGE___', image)
+            .replace('___IMAGE___', image) \
+            .replace('___CAMPID___', folder)
 
     # column closing tag
     if ((camp_number % col_len) == 0 or camp_number == len(camps)):
