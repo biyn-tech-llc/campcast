ba3a4428e906b5873f778d2a0445b15b6585e9ce
Munene Kiruja <enenum@yahoo.com>
2019-04-17 01:40:04 -0700

    destroy instead of toggle

--- methods.js
+++ methods.js
@@ -46,7 +46,7 @@ function copyTextToClipboard(text, element_id) {
 var timeout;
 function mouseOut(element_id) {
     clearTimeout(timeout);
-    $('[data-toggle="tooltip"]').tooltip('toggle')
+    $('[data-toggle="tooltip"]').tooltip('destroy')
 }
 
 $(document).ready(function(){
