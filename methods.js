322e56d93e1be93cfddfe46936d5b7b6187160f6
Munene Kiruja <enenum@yahoo.com>
2019-04-18 00:49:26 -0700

    log found elements

--- methods.js
+++ methods.js
@@ -34,6 +34,7 @@ function fallbackCopyTextToClipboard(text, element_id) {
 function copyTextToClipboard(text, element_id) {
   //if (!navigator.clipboard) {
     fallbackCopyTextToClipboard(text, element_id);
+    console.log($('element_id').length + ' elements found');
     $('#element_id').tooltip({title: 'Copied RSS feed. Go paste it into your podcast app', trigger: 'manual', placement: 'top'})
     $('#element_id').tooltip('show');
     setTimeout(function(){$('#element_id').tooltip('hide')}, 3000);
