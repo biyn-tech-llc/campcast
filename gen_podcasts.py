6e20bd8e2ebb80a795b3341fa07d8c1e9888c700
enenum <enenum@users.noreply.github.com>
2019-04-18 23:59:26 -0700

    Update gen_podcasts.py
    try bootstrap bundle

--- gen_podcasts.py
+++ gen_podcasts.py
@@ -191,7 +191,7 @@ list_page_header = '''
   <link rel="stylesheet" type="text/css" href="style/inline.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 
 </head>
 
