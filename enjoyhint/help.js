0ffc2bb50017bf7871bf00ea6d2be2f27e2246a8
Munene Kiruja <enenum@yahoo.com>
2019-05-13 23:47:45 -0700

    fix help hints

--- enjoyhint/help.js
+++ enjoyhint/help.js
@@ -5,7 +5,7 @@
 	 "next #idthis": 'Each camp in <b>The Machaneh Directory</b> is a podcast on its own - a powerful, unique and immersive spiritual experience.'
  },
  {
-	 "next #loyalty_and_the_mega_church":"<text style='text-align: left'>Two easy steps:<br>" +
+	 "next #building_a_multiple_mega_church":"<text style='text-align: left'>Two easy steps:<br>" +
 		"<li>First click or tap a camp thumbnail - its URL is copied.</li>" + 
 		"<li>Then subscribe to it by URL in your podcast app</li>",
       'shape': 'circle',
@@ -18,7 +18,7 @@
     "next #idsearch": 'Looking for a particular camp?<br>Scroll down for more thumbnails, or enter part of the name in search bar.'
  },
  {
-    "click #idsearch": 'Consider adding this page to your home screen (if your browser is capable) for future easy a ccess.',
+    "click #idsearch": 'Consider adding this page to your home screen for easy access in the future.',
     "skipButton": {className: "sqip", text: "Got It!"}
  },
  ];
