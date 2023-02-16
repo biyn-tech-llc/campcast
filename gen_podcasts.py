32ae6e7a3ee815eb2977d21f37e6e6762dea53d5
Munene Kiruja <enenum@yahoo.com>
2023-02-15 19:14:55 -0800

    add 2022 camps

--- gen_podcasts.py
+++ gen_podcasts.py
@@ -133,7 +133,7 @@ camps = [
     ('And Ye Shall Compass The City', 'C123_', 'and%20ye%20shall.jpg', '2018-10-31.05'),
     ('No City Shall Be Too Strong For You 2018', 'C123_', 'no%20city%20shall%20be%20too%20strong%20for%20you.jpg', '2018-10-31.07'),
     #('Life In The Church', 'C124_'), #repeat of C19_
-    ('Season of Withdrawal','','1550854868_season%20of%20withdrawal.jpg','',b'1.%20Prophets%20or%20Spirituals.mp3 \
+    ('Season of Withdrawal','C130_','1550854868_season%20of%20withdrawal.jpg','',b'1.%20Prophets%20or%20Spirituals.mp3 \
                                                                               2.%20Prophets%20or%20Spirituals_Prayer%20Session.mp3 \
                                                                               3.%20The%20Fall%20of%20Lucifer.mp3 \
                                                                               4.%20The%20Fall%20of%20Lucifer_Prayer%20Session%20.mp3 \
@@ -175,10 +175,61 @@ camps = [
                                                                               40.%20Awake%20O%20Sleeper.mp3'),
     ('I Only Need One Talent', 'C125_', 'one%20talent.jpg' ),
     ('Make Yourself A Saviour of Men - Harmattan Bible Seminar', 'C120%20', '1550945225_make%20yourself.jpg'),
-    ('Jesus! Saviour of the World', 'C130_', 'JESUS%20SAVIOUR%20OF%20THE%20LORD.jpg', '2019-08-28'),
     ('I Come Seeking Fruit', 'C126_', 'i_come_looking_fruits.jpg'),
     ('Am I Good For Nothing','C128_', '128.%20Am_i_good.jpg'),
-    ('Choose Me Use Me','C130_', '130.%20Choose_me_use_me.jpg', '2019-10-29'),
+    ('Jesus! Saviour of the World', '', 'JESUS%20SAVIOUR%20OF%20THE%20LORD.jpg', '',b'C130_01%20Opening%20Prayer%20with%20Rev.%20Frank.mp3 \
+                                                                                           C130_02.%20Introduction%20by%20Bishop%20Ogoe.mp3 \
+                                                                                           C130_03.%20How%20You%20See.mp3 \
+                                                                                           C130_04.%20Seeing%20And%20Knowing.mp3 \
+                                                                                           C130_05.%20Prayers%20With%20Rev%20Frank.mp3 \
+                                                                                           C130_06.%20Who%20Is%20Jesus_%20He%20Is%20The%20Saviour%20Of%20The%20World.mp3 \
+                                                                                           C130_07.%20Jesus%20Is%20Our%20Hope.mp3 \
+                                                                                           C130_08.%20Worship%20And%20Prayer%20For%20Our%20Salvation.mp3 \
+                                                                                           C130_09.%20Jesus%20Is%20The%20Son%20Of%20Man.mp3 \
+                                                                                           C130_10.%20Jesus%20Is%20The%20Son%20of%20God.mp3 \
+                                                                                           C130_11.%20Jesus%20Is%20The%20Way,%20The%20Truth%20And%20The%20Life.mp3 \
+                                                                                           C130_12.%20Prayers%20Of%20Thanksgiving%20With%20Rev.%20Frank.mp3 \
+                                                                                           C130_13.%20Jesus%20Is%20The%20Word.mp3 \
+                                                                                           C130_14.%20Worship%20And%20Prayer%20time.mp3 \
+                                                                                           C130_15.%20Jesus%20Is%20The%20Lamb%20That%20Takes%20Away%20The%20Sins%20Of%20The%20World.mp3 \
+                                                                                           C130_16.%20Jesus\'s%20Blood%20Cleanses%20Us%20Of%20Our%20Sins.mp3 \
+                                                                                           C130_17.%20Prayers%20With%20Rev.%20Frank.mp3 \
+                                                                                           C130_18.%20Jesus%20Is%20The%20Anointed%20One.mp3 \
+                                                                                           C130_19.%20The%20Spirit%20Of%20The%20Anti-Christ%20Is%20The%20Spirit%20Of%20The%20Anti-Anointing.mp3 \
+                                                                                           C130_20.%20Prayers%20With%20Rev.%20Frank.mp3 \
+                                                                                           C130_21.%20Recap%20With%20Bishop%20Ogoe.mp3 \
+                                                                                           C130_22.%20Who%20Is%20Jesus.mp3 \
+                                                                                           C130_23.%20Prayers%20For%20The%20Holy%20Spirit%20With%20Rev%20Frank.mp3 \
+                                                                                           C130_24.%20The%20Uniqueness%20Of%20Jesus%20Christ.mp3 \
+                                                                                           C130_25.%20How%20Jesus%20Saved%20Us.mp3 \
+                                                                                           C130_26.%20Recap%20By%20Bishop%20Ogoe.mp3 \
+                                                                                           C130_27.%20Know%20Jesus%20and%20What%20He%20Has%20Done%20For%20you.mp3 \
+                                                                                           C130_28.%20Go%20Ye%20Into%20The%20World.mp3 \
+                                                                                           C130_30.%20Prayers%20With%20Rev%20Frank.mp3 \
+                                                                                           C130_31.%20Recap%20With%20Bishop%20Ogoe.mp3 \
+                                                                                           C130_32.%20Bear%20Much%20Fruits%20In%20Every%20Continent.mp3 \
+                                                                                           C130_33.%20Don\'t%20Work%20For%20Money.mp3 \
+                                                                                           C130_34.%20Become%20A%20Branch.mp3 \
+                                                                                           C130_35.%20Become%20A%20Teacher.mp3 \
+                                                                                           C130_36.%20Become%20A%20Person%20Of%20Speed.mp3 \
+                                                                                           C130_37.%20Don\'t%20Be%20Like%20The%20World.mp3 \
+                                                                                           C130_38.%20Recap%20By%20Bishop.mp3 \
+                                                                                           C130_39.%20Brothers%20Be%20Strong.mp3 \
+                                                                                           C130_40.%20Sister%20Be%20Spiritual.mp3 \
+                                                                                           C130_41.%20The%20Prophecies%20Of%20Your%20Destiny.mp3'),
+    ('Choose Me Use Me','C130_', '130.%20Choose_me_use_me.jpg', '', b'C130_00%20Intro.%20Choose%20Me.mp3 \
+                                                                 C130_01.%20The%20Parable%20of%20The%20Seed.mp3 \
+                                                                 C130_02.%20Choose%20Me,%20Use%20Me%20-%20Introduction.mp3 \
+                                                                 C130_03.%20What%20Does%20It%20Mean%20For%20God%20To%20Choose%20Or%20Use%20You.mp3 \
+                                                                 C130_04.%20Those%20Who%20Are%20Chosen.mp3 \
+                                                                 C130_05.%20What%20is%20Faith.mp3 \
+                                                                 C130_06.%20Those%20Who%20Are%20Humble.mp3 \
+                                                                 C130_07.%20Why%20Should%20God%20Choose%20You.mp3 \
+                                                                 C130_08.%20Grow%20To%20Honour%20Your%20Prophet.mp3 \
+                                                                 C130_09.%20Camp%20Testimony.mp3 \
+                                                                 C130_10.%20Closing%20Session.mp3'),
+    ('Eunuchs In The Palace', 'C133_', 'eunuchs_in_the_palace-min.jpg'),
+    ('Use It Or Lose It', 'C135_'),
 ]
 FILES_URL="http://daghewardmillsaudio.org/songs/"
 LINK_URL="https://www.machanehcast.com/"
@@ -454,14 +505,14 @@ for camp in camps:
         image_file = camp[2]
         image = IMAGES_URL + image_file
         print(image)
-        response = requests.get(image)
+        response = requests.get(image, headers={"User-Agent": "XY"})
     else:
         cmd = "grep -i -m 1 " + name.replace(' ', '.*') + " images.html"
         try:
             image_line = subprocess.check_output(cmd.split())
             image_file = re.search(r'(?<=href=").*jpg(?=">)', str(image_line)).group(0)
             image = IMAGES_URL + image_file
-            response = requests.get(image.replace("\\'","'"))
+            response = requests.get(image.replace("\\'","'"), headers={"User-Agent": "XY"})
             if response.status_code != 200:
                 print ('not found: ' + image)
                 image = LINK_URL + 'DAG.jpg'
